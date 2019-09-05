bbdb18d086aaaf159aee0429389b75242480419d
Sergey Koval <s.sergeykoval@gmail.com>
2019-09-05 17:07:43 +0300

    Default screenshots folder path fix;

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,7 @@ const env = dotenv.config().parsed || process.env;
 const config = {
   app: {
     port: process.env.PORT || env.SOCKET_PORT || 6001,
-    screenshotsFolder: env.SCREENSHOTS_FOLDER || os.homedir() + '.screenshots/'
+    screenshotsFolder: env.SCREENSHOTS_FOLDER || os.homedir() + '/.screenshots/'
   },
 };
 
