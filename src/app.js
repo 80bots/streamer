9795e648b124fd53bc2b1760f41f3dfcb19625d7
Sergey Koval <s.sergeykoval@gmail.com>
2019-09-17 13:51:59 +0300

    Simple middleware added;

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import config from './config';
 import SendScreenshots from './tasks/SendScreenshots';
 import SendLogs from './tasks/SendLogs';
 import SendOutput from './tasks/SendOutput';
+import valid from './middlewares/auth';
 import { getLogger } from './services/logger';
 import { init } from './services/socket.io';
 
@@ -16,11 +17,13 @@ const initApp = () => {
   logger.info('Initializing socket server...');
   const socket = init();
   socket.on('connect', (socket) => {
-    logger.info(`${socket.id} connected`);
-    new SendScreenshots(socket);
-    new SendLogs(socket);
-    new SendOutput(socket);
-    socket.on('disconnect', () => logger.info(`${socket.id} disconnected`));
+    if(valid(socket)) {
+      logger.info(`${socket.id} connected`);
+      new SendScreenshots(socket);
+      new SendLogs(socket);
+      new SendOutput(socket);
+      socket.on('disconnect', () => logger.info(`${socket.id} disconnected`));
+    }
   });
   socket.listen(config.app.port);
   logger.info(`Socket server is listening on port ${config.app.port}`);
---
+++ src/middlewares/auth.js
@@ -0,0 +1,17 @@
+import { execSync } from 'child_process';
+
+export default function (socket) {
+  if(process.env !== 'production') return true;
+  if(socket.request._query.id) {
+    try {
+      let id = execSync('curl http://169.254.169.254/latest/meta-data/instance-id');
+      if (id === socket.request._query.id) return true;
+    } catch (e) {
+      socket.disconnect();
+      return false;
+    }
+  } else {
+    socket.disconnect();
+    return false;
+  }
+}
