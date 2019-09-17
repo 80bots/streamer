6fcdf401265c4b9d78b39ebacd263d17c644c0a5
Sergey Koval <s.sergeykoval@gmail.com>
2019-09-17 15:54:59 +0300

    Simplified env check;

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,6 @@
 import http from 'http';
 
-const requestId = new Promise((resolve, reject) => {
+const requestId = () => new Promise((resolve, reject) => {
   http.get('http://169.254.169.254/latest/meta-data/instance-id', (resp) => {
     let data = '';
     resp.on('data', chunk => { data += chunk; });
@@ -9,7 +9,7 @@ const requestId = new Promise((resolve, reject) => {
 });
 
 export default async function (socket) {
-  if(process.env.NODE_ENV && process.env.NODE_ENV !== 'production') return true;
+  if(process.env.NODE_ENV !== 'production') return true;
   if(socket.request._query.id) {
     try {
       let id = await requestId().catch(() => '');
