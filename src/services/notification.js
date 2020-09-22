36d68f4ee46d440206ba2305a45269d4467139f8
romfis <roman.bikbaiev.dev@gmail.com>
2020-09-22 14:59:15 +0300

    Date field has been added.

--- src/services/notification.js
+++ src/services/notification.js
@@ -1,6 +1,7 @@
 import Echo from "laravel-echo";
 import io from "socket.io-client";
 import config from "../config";
+import dayjs from "dayjs"
 
 class Notification {
   connect() {
@@ -18,8 +19,9 @@ class Notification {
   }
 
   emit(chat, status) {
-    // console.log(chat, {...status,  instanceId: config.instance.id});
-    this.notification.whisper(chat, {...status, instanceId: config.instance.id});
+    const date = dayjs().format("YYYY-MM-DD HH:mm:ss");
+    //console.log(chat, {...status,  instanceId: config.instance.id, date: date});
+    this.notification.whisper(chat, {...status, instanceId: config.instance.id, date: date});
   }
   on(chat, callback) {
     this.notification.listenForWhisper(chat, callback);
