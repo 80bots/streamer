f9c19ef732ca22c55bb1fd6ae62ba2493d178dc0
Francis Kim <francis@80bots.com>
2021-02-22 08:50:35 +1100

    change to 24 hour format

--- src/resources-listeners/ScreenShots.js
+++ src/resources-listeners/ScreenShots.js
@@ -1,4 +1,4 @@
-import { watch } from "chokidar";
+import {watch} from "chokidar";
 import appConfig from "../config";
 import fs from "fs";
 import Path from "path";
@@ -6,61 +6,61 @@ import dayjs from "dayjs";
 import getColors from "get-image-colors";
 
 class Listener {
-  constructor() {
-    this.storageRoot = appConfig.local.root;
-    this.root = appConfig.app.screenshotsFolder;
-    this.folder = dayjs().format("YYYY-MM-DD hh:mm:ss");
-    const interval = setInterval(() => {
-      if (fs.existsSync(this.root)) {
-        this.watcher = watch(this.root, {
-          persistent: true,
-          ignoreInitial: true
-        });
-        this.applyListeners();
-        clearInterval(interval);
-      } else {
-        // console.log(`${this.root} doesn't exist`);
-      }
-    }, 1000);
-  }
-
-  applyListeners() {
-    this.watcher
-      .on("add", (...params) => this.filterScreenshot(...params))
-      .on("change", (...params) => this.filterScreenshot(...params));
-  }
-
-  filterScreenshot(path) {
-
-    const blackScreenshot = [ '#fcfcfc', '#040404', '#c0ff80', '#408480', '#400484' ];
-    const fileName = Path.basename(path);
-
-    getColors(path).then(colors => {
-      const screenshotColors = colors.map(color => color.hex());
-
-      if(screenshotColors.length !== blackScreenshot.length) {
-        this.onFileAdded(path, fileName);
-      } else if (!blackScreenshot.every((item, index) => item === screenshotColors[index])) {
-        this.onFileAdded(path, fileName);
-      } else {
-        this.onFileAdded(path, "blank_screenshot " + fileName);
-      }
-
-    }).catch(e => e);
-  }
-
-  onFileAdded(path, fileName) {
-
-    const link = `${this.storageRoot}/screenshots/${this.folder}/${fileName}`;
-
-    if (!fs.existsSync(Path.dirname(link))) {
-      fs.mkdirSync(Path.dirname(link), { recursive: true });
+    constructor() {
+        this.storageRoot = appConfig.local.root;
+        this.root = appConfig.app.screenshotsFolder;
+        this.folder = dayjs().format("YYYY-MM-DD HH:mm:ss");
+        const interval = setInterval(() => {
+            if (fs.existsSync(this.root)) {
+                this.watcher = watch(this.root, {
+                    persistent: true,
+                    ignoreInitial: true
+                });
+                this.applyListeners();
+                clearInterval(interval);
+            } else {
+                // console.log(`${this.root} doesn't exist`);
+            }
+        }, 1000);
     }
 
-    if (!fs.existsSync(link)) {
-      fs.symlinkSync(path, link);
+    applyListeners() {
+        this.watcher
+            .on("add", (...params) => this.filterScreenshot(...params))
+            .on("change", (...params) => this.filterScreenshot(...params));
     }
-  }
+
+    filterScreenshot(path) {
+
+        const blackScreenshot = ['#fcfcfc', '#040404', '#c0ff80', '#408480', '#400484'];
+        const fileName = Path.basename(path);
+
+        getColors(path).then(colors => {
+            const screenshotColors = colors.map(color => color.hex());
+
+            if (screenshotColors.length !== blackScreenshot.length) {
+                this.onFileAdded(path, fileName);
+            } else if (!blackScreenshot.every((item, index) => item === screenshotColors[index])) {
+                this.onFileAdded(path, fileName);
+            } else {
+                this.onFileAdded(path, "blank_screenshot " + fileName);
+            }
+
+        }).catch(e => e);
+    }
+
+    onFileAdded(path, fileName) {
+
+        const link = `${this.storageRoot}/screenshots/${this.folder}/${fileName}`;
+
+        if (!fs.existsSync(Path.dirname(link))) {
+            fs.mkdirSync(Path.dirname(link), {recursive: true});
+        }
+
+        if (!fs.existsSync(link)) {
+            fs.symlinkSync(path, link);
+        }
+    }
 
 }
 
